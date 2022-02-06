363c7f53a236b9554e9826c69bb8e0d2ea753837
JalenNM <71479385+JalenNM@users.noreply.github.com>
2022-02-05 22:01:41 -0500

    Initial commit

---
+++ public/scripts/app.js
@@ -0,0 +1,12 @@
+/* app.js - Jalen Mendonca - 301098614 - 02/05/2022 */
+
+// IIFI -- Immediately Invoked Function Expression
+(function(){
+
+    function start(){
+        console.log("App started...");
+    }
+
+    window.addEventListener("load", start);
+    
+})();
